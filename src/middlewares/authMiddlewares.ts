4d6c98029958d1ca79c18a3fbf6750c7c166b477
Hưng Sam <minhhung.sam@gmail.com>
2022-03-31 16:55:56 +1100

    add unauthorized.ejs

--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import config from '../config';
+const { SITE_TITLE } = config;
 
 export const requireAdmin = (req: Request, res: Response, next: NextFunction) => {
     if ( req.isAuthenticated() && req.user.isAdmin ) {
         next();
     } else {
-        res.status(401).json({message: 'Admin level authorization is required to access this resource'});
+        res.render('unauthorized', {title: SITE_TITLE, message: 'Admin level authorization is required to access this resource'});
     }
 }
